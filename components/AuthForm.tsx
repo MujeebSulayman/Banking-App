910c7c8a19801753c655a93ac339020a1d0c1a2e
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-05-15 19:19:31 +0100

    innit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,13 +1,52 @@
-'use client'
+'use client';
+
 
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react'
+import { useState } from 'react';
 
-const AuthForm = ({type}: {type: string} ) => {
 
-    const [user, setUser] = useState(null)
-  return (
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+	username: z.string().min(2, {
+		message: 'Username must be at least 2 characters.',
+	}),
+});
+
+const AuthForm = ({ type }: { type: string }) => {
+	const [user, setUser] = useState(null);
+
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: '',
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
+	return (
 		<section className='auth-form'>
 			<header className='flex flex-col gap-5 md:gap-8'>
 				<Link
@@ -23,14 +62,51 @@ const AuthForm = ({type}: {type: string} ) => {
 						HemBanking
 					</h1>
 				</Link>
-                <div className='flex flex-col md:gap-3 gap-1'>
-                    <h1>
-                        {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign-Up'}
-                    </h1>
-                </div>
+				<div className='flex flex-col md:gap-3 gap-1'>
+					<h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
+						{user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign-Up'}
+						<p className='text-16 font-normal text-gray-600'>
+							{user
+								? 'Link your account to get started'
+								: 'Please enter your details'}
+						</p>
+					</h1>
+				</div>
+				{user ? (
+					<div className='flex flex-col gap-4'>{/* PlaidLink */}</div>
+				) : (
+					<>
+						<Form {...form}>
+							<form
+								onSubmit={form.handleSubmit(onSubmit)}
+								className='space-y-8'>
+								<FormField
+									control={form.control}
+									name='username'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Username</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='shadcn'
+													{...field}
+												/>
+											</FormControl>
+											<FormDescription>
+												This is your public display name.
+											</FormDescription>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<Button type='submit'>Submit</Button>
+							</form>
+						</Form>
+					</>
+				)}
 			</header>
 		</section>
 	);
-}
+};
 
-export default AuthForm
+export default AuthForm;
