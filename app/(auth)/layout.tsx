d1c267b4cd2efe202f0df837508ed056580c54d0
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-05-28 10:37:31 +0100

    Auth layout implemented

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
 					<Image
 						src='/icons/auth-image.svg'
 						alt='auth image'
-						width={500}
-						height={500}
+						width={450}
+						height={400}
 					/>
 				</div>
 			</div> 
